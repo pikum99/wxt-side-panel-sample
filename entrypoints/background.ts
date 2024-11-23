498489443bd5d38f9531535eef8a16fd7fe66c6d
pikum <drryuuzaki@gmail.com>
2024-11-23 21:46:24 +0900

    サイドパネル閉鎖検出機能を追加し、onDisconnectを利用したフラグ管理を実装

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,28 +1,30 @@
 export default defineBackground(() => {
-  // console.log("Hello background!", { id: browser.runtime.id });
-  // サイドパネルが開いているかどうかを管理するフラグ
   let isSidePanelOpen = false;
 
   // ボタンがクリックされたときの処理
   chrome.action.onClicked.addListener((tab) => {
-    // console.log("Button clicked!", { id: browser.runtime.id });
     if (isSidePanelOpen) {
-      // サイドパネルを閉じるメッセージを送信
-      // 参考: https://github.com/w3c/webextensions/issues/521
       chrome.runtime.sendMessage("closeSidePanel");
     } else {
-      // サイドパネルを開く
       chrome.sidePanel.open({ windowId: tab.windowId });
     }
-    // フラグをトグル
     isSidePanelOpen = !isSidePanelOpen;
+  });
+
+  // サイドパネルの状態を検出する
+  chrome.runtime.onConnect.addListener((port) => {
+    if (port.name === "mySidepanel") {
+      // サイドパネルが閉じられたときの処理
+      port.onDisconnect.addListener(() => {
+        console.log("Sidepanel closed.");
+        isSidePanelOpen = false; // フラグをリセット
+      });
+    }
   });
 
   // サイドパネルを閉じる処理をサイドパネルスクリプトに委託
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message === "closeSidePanel") {
-      // console.log("Closing side panel...");
-      // サイドパネルにメッセージを送る処理（後述の sidepanel.js に関連）
       sendResponse({ action: "close" });
     }
   });
